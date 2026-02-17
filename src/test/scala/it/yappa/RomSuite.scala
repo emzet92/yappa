8595d16304e7b942a35eb6f330b46a24f5e02da4
mateusz <mzalewski1992@gmail.com>
2026-02-17 11:38:17 +0100

    Add data test example

--- src/test/scala/it/yappa/RomSuite.scala
+++ src/test/scala/it/yappa/RomSuite.scala
@@ -6,4 +6,16 @@ class RomSuite extends FunSuite {
   test("some test") {
     assert(true)
   }
+
+  val cases = List(
+    (1, 1, 2),
+    (2, 3, 5),
+    (10, 5, 15)
+  )
+
+  cases.foreach { (a, b, expected) =>
+    test(s"$a + $b should equal $expected") {
+      assertEquals(a + b, expected)
+    }
+  }
 }
