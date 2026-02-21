e76a99e829945410cf62177d36b0d5d7aa86abc5
mateusz <mzalewski1992@gmail.com>
2026-02-21 14:04:32 +0100

    Catz configured

--- build.sbt
+++ build.sbt
@@ -7,8 +7,29 @@ lazy val root = (project in file("."))
   .settings(
     name := "yappa"
   )
-libraryDependencies +=
-  "org.scalameta" %% "munit" % "1.2.2" % Test
+libraryDependencies ++= Seq(
+  // Test
+  "org.scalameta" %% "munit" % "1.2.2" % Test,
 
+  // Cats
+  "org.typelevel" %% "cats-core" % "2.13.0",
+  "org.typelevel" %% "cats-effect" % "3.6.3",
+
+  // http4s (Ember server)
+  "org.http4s" %% "http4s-ember-server" % "0.23.33",
+  "org.http4s" %% "http4s-dsl" % "0.23.33",
+  "org.http4s" %% "http4s-core" % "0.23.33",
+  "org.http4s" %% "http4s-circe" % "0.23.33",
+
+  // Circe
+  "io.circe" %% "circe-generic" % "0.14.15",
+
+  // Doobie
+  "org.tpolecat" %% "doobie-core" % "1.0.0-RC11",
+  "org.tpolecat" %% "doobie-hikari" % "1.0.0-RC11",
+
+  // SQLite
+  "org.xerial" % "sqlite-jdbc" % "3.51.1.0"
+)
 
 assembly / assemblyJarName := "planning-poker-app.jar"
--- src/main/scala/it/yappa/Main.scala
+++ src/main/scala/it/yappa/Main.scala
@@ -1,19 +1,71 @@
 package it.yappa
 
+import cats.effect.{Clock, IO, IOApp}
+import com.comcast.ip4s.{host, port}
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.*
+import org.http4s.dsl.io.*
+
 import java.time.Instant
 import java.util.UUID
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    val mateusz = Participant(
-      ParticipantId(UUID.randomUUID()),
-      "Mateusz",
-      true)
 
-    val stringOrRoom = Room.create("some-session")
-      .addParticipant(mateusz)
-      .startVoting(Instant.now())
+object Main extends IOApp.Simple:
 
-    println(stringOrRoom)
-  }
-}
+  val logo =
+    """
+      |  _   _  __ _ _ __  _ __   __ _
+      | | | | |/ _` | '_ \| '_ \ / _` |
+      | | |_| | (_| | |_) | |_) | (_| |
+      |  \__, |\__,_| .__/| .__/ \__,_|
+      |   __/ |     | |   | |
+      |  |___/      |_|   |_|
+          """.stripMargin
+
+  private val routes: HttpRoutes[IO] = HttpRoutes.of[IO]:
+    case GET -> Root =>
+      Ok("OK: scala3 + cats + http4s\n")
+
+    case GET -> Root / "health" =>
+      Ok("healthy")
+
+    case req@POST -> Root / "echo" =>
+      req.as[String].flatMap(body => Ok(body))
+
+  private val app: HttpApp[IO] =
+    routes.orNotFound
+
+  val mateusz = Participant(
+    ParticipantId(UUID.randomUUID()),
+    "Mateusz",
+    true)
+
+  val stringOrRoom = Room.create("some-session")
+    .addParticipant(mateusz)
+    .startVoting(Instant.now())
+  println(stringOrRoom)
+
+  override def run: IO[Unit] =
+    for
+      start <- Clock[IO].monotonic
+      _ <- IO.println(logo)
+      _ <- IO.println("Starting HTTP server...")
+
+      _ <- EmberServerBuilder
+        .default[IO]
+        .withHost(host"0.0.0.0")
+        .withPort(port"8080")
+        .withHttpApp(app)
+        .build
+        .evalTap { _ =>
+          for
+            end <- Clock[IO].monotonic
+            took = (end - start).toMillis
+            _ <- IO.println(
+              s"HTTP server started in ${took} ms 🚀"
+            )
+          yield ()
+        }
+        .useForever
+    yield ()
+
