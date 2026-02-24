795f41bdc124d6781bd5a9ad438aa789a723e976
mateusz <mzalewski1992@gmail.com>
2026-02-24 15:18:51 +0100

    Disable graalpy. Move it later to scadatomic runtime

--- src/main/scala/it/yappa/Main.scala
+++ src/main/scala/it/yappa/Main.scala
@@ -9,7 +9,7 @@ import org.http4s.circe.*
 import io.circe.*
 import io.circe.generic.semiauto.*
 import it.yappa.Room.{CreateRoomRequest, SubmitVoteRequest}
-import org.graalvm.polyglot.Context
+//import org.graalvm.polyglot.Context
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 
 // ===== JSON request decoding =====
@@ -19,14 +19,14 @@ given Decoder[SubmitVoteRequest] = deriveDecoder
 given EntityDecoder[IO, SubmitVoteRequest] = jsonOf[IO, SubmitVoteRequest]
 
 object Main extends IOApp.Simple:
-  val ctx = Context.newBuilder("python")
-    .allowAllAccess(true)
-    .build()
+//  val ctx = Context.newBuilder("python")
+//    .allowAllAccess(true)
+//    .build()
+//
+//  val result = ctx.eval("python", "sum([1,2,3])")
+//  ctx.eval("python", "print('Hello from Python!')")
 
-  val result = ctx.eval("python", "sum([1,2,3])")
-  ctx.eval("python", "print('Hello from Python!')")
-
-  println(result.asInt())
+//  println(result.asInt())
 
   val logo =
     """
@@ -110,7 +110,7 @@ object Main extends IOApp.Simple:
       start <- Clock[IO].monotonic
       _ <- IO.println(logo)
       _ <- IO.println("Starting HTTP server...")
-      _ <- IO.println(s"Hello from python: $result")
+//      _ <- IO.println(s"Hello from python: $result")
       planningPoker <- PlanningPoker.create[IO]
 
       _ <- EmberServerBuilder
